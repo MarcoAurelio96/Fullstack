ad4c08c849ad484399445b957d286e72275222c0
MarcoAurelio96 <MarcoAureliolopezcubo@gmail.com>
2026-04-20 11:29:46 +0200

    Registrada la aplicación en la web Firebase. Obtenidas las keys y habilitado el modo Authentication

--- hybrid-hub/src/config/firebase.ts
+++ hybrid-hub/src/config/firebase.ts
@@ -1,15 +1,19 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyDfq480PuTvpxW5h2DfPyBRjAHfuWcF9l8",
+  authDomain: "ironpace-3f9de.firebaseapp.com",
+  projectId: "ironpace-3f9de",
+  storageBucket: "ironpace-3f9de.firebasestorage.app",
+  messagingSenderId: "687023895456",
+  appId: "1:687023895456:web:2a4028c36c7ee944d3aac5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
